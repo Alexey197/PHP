05b8ddb8bdc1b6296f28cc422a67379cbe7c6b0b
Alexey197 <margadonnn3@gmail.com>
2022-04-17 19:02:49 +0300

    improvements

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const title1 = document.querySelector('h1')
 const title2 = document.querySelector('h2')
 
+let data1 = title1.dataset.time
+
 if (title1.dataset.time === '1') {
   title2.textContent = 'Утро'
-} else if (title1.dataset.time === '2') {
+} else if (data1 === '2') {
   title2.textContent = 'День'
-} else if (title1.dataset.time === '3') {
+} else if (data1 === '3') {
   title2.textContent = 'Вечер'
-} else if (title1.dataset.time === '4') {
+} else if (data1 === '4') {
   title2.textContent = 'Ночь'
 }
