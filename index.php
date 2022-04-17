b31df0fc47ebba0f5b02ad0d6726c612256bdc6d
Alexey197 <margadonnn3@gmail.com>
2022-04-17 14:38:09 +0300

    second commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+  $h = date('H') + 3;
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+    <style>
+        body{
+          background: url(img/<?php echo $h % 4;?>.jpg);
+          background-size: cover;
+          color: red;
+        }
+    </style>
+</head>
+  <body>
+  <h1><?php echo $h?></h1>
+  <p>lorem</p>
+  <p>lorem</p>
+  <p>lorem</p>
+  <p>lorem</p>
+  <p>lorem</p>
+
+</body>
+</html>
