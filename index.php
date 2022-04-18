b18550e4a76a38b56f2829fda2a65fb67c2d3517
Alexey197 <margadonnn3@gmail.com>
2022-04-18 18:23:08 +0300

    improve

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
   $h = date('H') + 3;
 //  $img = ($h / 6) % 4
-  $img = (int)($h / 6)
+  $img = (int)($h / 6);
+  $time = '';
 ?>
 
 <!doctype html>
@@ -15,19 +16,32 @@
     <style>
         body{
           background: url(img/<?php echo $img;?>.jpg);
-          background-size: cover;
+          /*background-size: cover;*/
           color: red;
         }
     </style>
 </head>
   <body>
   <h1 data-time="<?php echo $img?>"><?php echo $h?></h1>
-  <h2></h2>
+  <h2>
+      <?php
+    if ($img == '1') {
+        $time = 'Утро';
+    } elseif ($img == '2') {
+        $time = 'День';
+    } elseif ($img == '3') {
+        $time = 'Вечер';
+    } elseif ($img == '4') {
+        $time = 'Ночь';
+    }
+      echo $time;
+    ?>
+  </h2>
   <p>lorem</p>
   <p>lorem</p>
   <p>lorem</p>
   <p>lorem</p>
   <p>lorem</p>
-<script src="index.js"></script>
+<!--<script src="index.js"></script>-->
 </body>
 </html>
